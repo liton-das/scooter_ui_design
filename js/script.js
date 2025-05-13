dc5d242f1efecf599219c9f03ef6fcf344acf4bf
liton-das <litonchandra094@gmail.com>
2025-05-14 00:16:59 +0300

    colors section added

--- js/script.js
+++ js/script.js
@@ -52,3 +52,20 @@ let wordflick = function () {
 $(document).ready(function () {
   wordflick();
 });
+// ------------------- slick slider ------------------------
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true
+  
+});

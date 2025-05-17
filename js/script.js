60e39d6c24accbb11ffd8e8703c3af4bfdb51878
liton-das <litonchandra094@gmail.com>
2025-05-17 17:47:58 +0300

    responsive completed for extra-small device

--- js/script.js
+++ js/script.js
@@ -77,4 +77,29 @@ $('.slider_card').slick({
   slidesToScroll: 3,
   prevArrow:`<i class="fa-solid arrow arrow-left fa-arrow-left"></i>`,
   nextArrow:`<i class="fa-solid arrow arrow-right fa-arrow-right"></i>`,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
